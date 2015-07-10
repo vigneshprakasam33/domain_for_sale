813f88a88aa5dfe25f19d225269056c0411fa37f
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2015-07-10 12:05:21 +0530

    pushover for offer notification

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
 require 'unicorn'
+require 'rest-client'
 
 class App   < Sinatra::Base
   get '/' do
-    redirect "index.html"
+    erb :index
+  end
+
+  post '/' do
+    RestClient.post "https://api.pushover.net/1/messages.json", :token => "ayUrGvK4xDvYewE7EFVXJCoMrCKeMx", :user => "nAmrvNBQ74LL9sErFPT3JiH1aquX6x", :device => "gt-i9300",
+                    :title => "The twilio offer", :message => "Email = #{params[:email]} , Offer = #{params[:offer]}"
   end
 end
