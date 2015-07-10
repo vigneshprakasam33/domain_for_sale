f4dd645e9e71be617b7a7c88de3f2ead1fbea297
Vignesh Prakasam <vigneshp.ceg+github@gmail.com>
2015-07-10 12:22:03 +0530

    redirect to index.html after post

--- app.rb
+++ app.rb
@@ -10,5 +10,6 @@ class App   < Sinatra::Base
   post '/' do
     RestClient.post "https://api.pushover.net/1/messages.json", :token => "ayUrGvK4xDvYewE7EFVXJCoMrCKeMx", :user => "nAmrvNBQ74LL9sErFPT3JiH1aquX6x", :device => "gt-i9300",
                     :title => "The twilio offer", :message => "Email = #{params[:email]} , Offer = #{params[:offer]}"
+    redirect "index.html"
   end
 end
